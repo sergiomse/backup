8f126d4e3938ddd126b9aefccd7b3d7ed5029926
sergiomse <sergiomse@gmail.com>
2017-09-07 19:49:53 +0100

    Added project panel and add button

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from "@angular/core";
+ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
 import {TitleComponent} from "./title.component";
@@ -7,10 +7,22 @@ import {InlineSVGModule} from "ng-inline-svg";
 import {HttpClientModule} from "@angular/common/http";
 import {SplitComponent} from "./split.component";
 import {ProjectPanelComponent} from "./project-panel.component";
+import {MatButtonModule, MatFab} from "@angular/material";
 
 @NgModule({
-    imports: [BrowserModule, InlineSVGModule, HttpClientModule],
-    declarations: [AppComponent, TitleComponent, RibbonComponent, SplitComponent, ProjectPanelComponent],
+    imports: [
+        BrowserModule,
+        InlineSVGModule,
+        HttpClientModule,
+        MatButtonModule
+    ],
+    declarations: [
+        AppComponent,
+        TitleComponent,
+        RibbonComponent,
+        SplitComponent,
+        ProjectPanelComponent
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
