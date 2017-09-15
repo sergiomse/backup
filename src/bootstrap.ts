df7395abef3cf3b6f4709ad094ff88fcc53613e0
sergiomse <sergiomse@gmail.com>
2017-09-15 19:14:24 +0100

    DOM Shadow change to NONE

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -3,5 +3,5 @@ import { AppModule }  from './app.module';
 import {ViewEncapsulation} from "@angular/core";
 
 platformBrowserDynamic().bootstrapModule(AppModule, [{
-    defaultEncapsulation: ViewEncapsulation.Native
+    defaultEncapsulation: ViewEncapsulation.None
 }]);
