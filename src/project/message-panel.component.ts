f7c519dc3ca708f84433b06564fd1dad1fde61ab
sergiomse <sergiomse@gmail.com>
2017-12-05 22:35:51 +0100

    Centered elements in message panel

--- src/project/message-panel.component.ts
+++ src/project/message-panel.component.ts
@@ -2,21 +2,25 @@ import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 
 @Component({
-    selector: 'empty-panel',
+    selector: 'message-panel',
     template: `
-        <div class="bk-empty-projects">
+        <div class="bk-message">
             <img [src]="imgUrl" width="128px" height="128px">
             <div class="bk-text">{{message}}</div>
         </div>`,
     styles: [`
-        .bk-empty-projects {
+        .bk-message {
             display: flex;
             flex-flow: column nowrap;
             align-items: center;
+            justify-content: center;
+            height: 100%;
+            overflow: hidden;
         }
 
         .bk-text {
             font-size: 2em;
+            text-align: center;
             margin-top: 30px;
             color: #8e8e8e;
         }`]
